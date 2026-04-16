d07a00eb186fa0bbf81c367f94da94da2872ed89
Saumya Rishikesh <164218039+Saumya1404@users.noreply.github.com>
2026-04-16 15:00:46 +0530

    Bulk sync: 11 submissions

---
+++ Data Structures & Algorithms/maximum-subarray/submission-0.py
@@ -0,0 +1,9 @@
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        max_sum,curr_sum = nums[0],0
+        for num in nums:
+            if curr_sum < 0:
+                curr_sum = 0
+            curr_sum += num
+            max_sum = max(max_sum,curr_sum)
+        return max_sum
---
+++ Data Structures & Algorithms/sort-an-array/submission-0.py
@@ -0,0 +1,4 @@
+class Solution:
+    def sortArray(self, nums: List[int]) -> List[int]:
+        sorted_list = sorted(nums,reverse = False)
+        return sorted_list
